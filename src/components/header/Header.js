41962a07c12de302159a6c66a4bd5e92ac0aee28
Audrey Thomasson <audrey@macbook-pro.lan>
2020-11-06 13:45:59 -0600

    updates to logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useHistory } from "react-router-dom"
 import { Button, Image, Header } from 'semantic-ui-react'
-import MtOutlineLogo from '../../images/MtOutlineLogo.png'
+import CapstoneLogo from '../../images/CapstoneLogo.png'
 import "../header/Header.css"
 
 
@@ -18,7 +18,7 @@ export const HeaderWelcome = () => {
     return (
     <>
         <div className='container'>
-        <Image src={MtOutlineLogo} size='small' wrapped className='item-a' />
+        <Image src={CapstoneLogo} size='small' wrapped className='item-a' />
         <Header as='h1' className='item-b'>TravelShots</Header>
         <div className='item-c'/>
         <Button  className='item-d' basic color='blue' size='tiny' onClick={clearStorage}>
